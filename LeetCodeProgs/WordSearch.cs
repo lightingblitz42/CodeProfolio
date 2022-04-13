09b398ecb356ea9782ede19fd7a6e84ad04d381f
lightingblitz42 <karenmalaska@Karens-MacBook-Air-2.local>
2022-04-12 19:56:39 -0700

    I fixed up my wrong LeetCode prog

--- LeetCodeProgs/WordSearch.cs
+++ LeetCodeProgs/WordSearch.cs
@@ -4,70 +4,116 @@ namespace calc.LeetCodeProgs
 {
     public static class WordSearch
     {
-        public static char[] f = new char[] { 'O', 'B', 'C', 'P'};
+        public static char[] f = new char[] { 'L', 'K', 'C', 'P' };
         public static List<char> k = new List<char> { 'O', 'B', 'C', 'P' };
         public static char[] c = new char[] { 'S', 'F', 'C', 'S' };
         public static char[] t = new char[] { 'A', 'D', 'E', 'E' };
-        public static char[][] board = new char[][] {f, c, t};
-        static string word = "OBIEHIFBH";
-        public static List<char> otherword = new List<char>(word.ToCharArray());
-        
+        public static char[][] board = new char[][] { f, c, t };
+        static string word = "SAUJFWYUTYCRFTDH";
 
+        public static List<char> otherwordfake = new List<char>(word.ToCharArray());
+        public static List<char> hashysjuner = new List<char>(otherwordfake);
+        public static List<char> hashys = new List<char>();
+        public static List<char> befored = new List<char>();
+        public static int hashycount = 0;
 
 
         public static void wordSearch()
         {
+
             for (int o = 0; o < board.Length; o++)
             {
-               List<char> dtherword = new List<char>(board[o]);
+                List<char> dtherword = new List<char>(board[o]);
                 Code(dtherword);
             }
-            Code(k);
 
-            if (otherword.Count <= 0)
+
+
+            for (int op = 0; op < hashysjuner.Count; op++)
             {
-                Console.WriteLine("True");
-
+                if (hashys.Contains(hashysjuner[op]))
+                {
+                    hashys.Remove(hashysjuner[op]);
+                }
+                else
+                {
+                    Console.WriteLine(hashysjuner[op] + "wduywudya");
+                    Console.WriteLine(false);
+                }
             }
-            else
+
+            if (hashycount >= hashysjuner.Count)
             {
-                Console.WriteLine("False");
-                foreach (var item in otherword)
+                foreach (var item in hashys)
                 {
                     Console.Write(item);
                 }
                 Console.WriteLine("");
+                Console.WriteLine(hashycount);
+                Console.WriteLine("True");
+
+
+
             }
-           
+            else
+            {
+
+                foreach (var item in hashys)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine("");
+                Console.WriteLine(hashycount + ", " + hashysjuner.Count);
+                Console.WriteLine(false);
+            }
+
         }
         public static void Code(List<char> Chocolate)
         {
-            
-               int Chocolatecount = Chocolate.Count;
-               int othercount = otherword.Count;
-               for (int i = 0; i < otherword.Count; i++)
-               {
-                if (otherword.Count == i)
+
+
+            int Chocolatecount = Chocolate.Count;
+            int othercount = otherwordfake.Count;
+            int ff = 0;
+            for (int i = 0; i < Chocolate.Count; i++)
+            {
+
+                if (othercount == ff)
                 {
+                    Console.WriteLine("d");
                     return;
                 }
-                for (int j = 0; j < Chocolate.Count; j++)
-                   {
-                    if (otherword.Count == i)
+                for (int j = 0; j < othercount; j++)
+                {
+                    ff = j;
+                    if (othercount == j)
                     {
+                        Console.WriteLine("d");
                         return;
                     }
-                    var er = Chocolate[j];
-                    var err = otherword[i];
-                    
-                    
-                    if(er == err)
+                    var er = Chocolate[i];
+                    var err = otherwordfake[j];
+
+
+                    if (er == err)
                     {
+
                         Console.WriteLine(er + " " + err);
-                      //    Chocolate.Remove(Chocolate[j]);
+
+
+                        hashycount++;
+                        Console.WriteLine(hashycount + ", ");
+                        hashys.Add(Chocolate[i]);
+                        break;
+                        //     Chocolate.Remove(Chocolate[j]);
                         //Chocolatecount--;
-                          otherword.Remove(otherword[i]);
+                        //otherword.Remove(otherword[j]);
                         //  othercount--;
+                    }
+                    else
+                    {
+                        Console.WriteLine(er + " " + err + "ldd" + j);
+
                     }
 
 
@@ -83,9 +129,15 @@ namespace calc.LeetCodeProgs
 
 
                 }
-               }
-            
+            }
+
 
         }
     }
 }
+
+
+
+
+
+
