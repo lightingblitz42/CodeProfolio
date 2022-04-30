e193e06773558b41de9d69f14a22b53d98f3b920
lightingblitz42 <karenmalaska@Karens-MacBook-Air-2.local>
2022-04-30 16:35:55 -0700

    Unfinished rightnow Currently im relearning the fundementals might take some time till next up load

--- Program.cs
+++ Program.cs
@@ -27,7 +27,8 @@ namespace calc
         public static int[] nums = new int[] {1,9,4,3,7,5,3,6,2 };
         public static void Main(string[] args)
         {
-            SortandFind.TargetIndices(nums, target);
+            GroupAnagrams.groupAnagrams();
+            //SortandFind.TargetIndices(nums, target);
             //findstartandendofprob.Findstartandendofprob();
            // calc._sumfindaddtothis.find();
             //calc.IsNumPandrlim.isNumPandrlim();
